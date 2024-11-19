d92aa18a5532752721ca99aa0cbeaff0a32db4a1
Albert Chambers <albertchambers09@gmail.com>
2024-11-19 14:09:50 +0200

    fix Header Navbar integration

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,24 +1,18 @@
 // @ts-check
 
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Navbar from './Navbar';
 
-/**
- * Header component with navigation links
- */
+
 export default function Header() {
   return (
     <header className="header">
+
       <div className="logo">
+        
       </div>
 
-      <nav>
-        <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          {/* Add more navigation links here as needed */}
-        </ul>
-      </nav>
+      <Navbar />
     </header>
   );
 }
